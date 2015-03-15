63d8293be414b5da432d2f8801c2390cf038001d
Justin Woo <moomoowoo@gmail.com>
2015-03-15 12:21:16 -0400

    pt 2 of the worst cljs ever known to man

--- src/home/core.cljs
+++ src/home/core.cljs
@@ -5,23 +5,23 @@
 (enable-console-print!)
 
 (defn nodelist-to-seq
-  "convert a nodelist to sequence NOW"
+  "convert a nodelist to sequence"
   [nodelist]
   (map #(.item nodelist %)
            (range nodelist.length)))
 
-(defn scream-your-innerText
-  "makes a node scream its innertext"
+(defn get-href
+  "get the href from the child of el"
   [el]
-  (println el.innerText))
+  (.attr (.children ($ el) 0) "href"))
 
-(defn set-active
+(defn set-active!
   "makes a node .active"
   [el]
   (let [$el ($ el)]
     (.addClass $el "active")))
 
-(defn unset-active
+(defn unset-active!
   "remove a node's .active"
   [el]
   (let [$el ($ el)]
@@ -31,6 +31,20 @@
   (nodelist-to-seq
     (js/document.querySelectorAll ".nav li")))
 
-(doseq [el nav-links]
-  (do (scream-your-innerText el)
-      (set-active el)))
+(defn set-active-nav!
+  "set the active nav element on the navbar"
+  []
+  (let [route js/window.location.hash]
+    (doseq [el nav-links]
+      (let [href (get-href el)]
+        (if
+          (or (= href route)
+              (and
+                (= href "#")
+                (= route "")))
+          (set-active! el)
+          (unset-active! el))))))
+
+(set! js/window.onhashchange set-active-nav!)
+
+(set-active-nav!)
